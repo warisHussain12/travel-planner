484f7808413fe5da13a2c214bb8a0adee49f6811
Waris Hussain <onlyhectic@gmail.com>
2025-05-08 22:09:10 +0530

    modified accomodation area in landing page

--- travel-planner/src/components/LandingPage.jsx
+++ travel-planner/src/components/LandingPage.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import tokyo2 from "../assets/tokyo2.webp";
 import { GoArrowUpRight } from "react-icons/go";
 import { FaArrowRight } from "react-icons/fa6";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 function LandingPage() {
   return (
     <div className="min-h-screen p-3 flex items-center justify-center bg-neutral-100 dark:bg-gray-800">
@@ -36,7 +44,7 @@ function LandingPage() {
               <p className="text-white">27.01.2025 - 02.02.2025</p>
             </div>
           </div>
-          <div className="bg-[#313DDF] text-white rounded-lg p-3 mt-3 border-4 border-[#3643FB] font-semibold">
+          <div className="bg-[#313DDF] text-white rounded-lg p-3 mt-3 border-[#3643FB] font-semibold">
             <div className="flex justify-between items-start">
               <div className="flex flex-col">
                 <p>Flight Details</p>
@@ -55,6 +63,39 @@ function LandingPage() {
                     <p className="text-lg">NRT</p>
                     <p>Narita, Tokyo</p>
                   </div>
+                </div>
+              </div>
+              <div>
+                <p className="underline text-xs">See all</p>
+              </div>
+            </div>
+          </div>
+          <div className="p-3 mt-3font-semibold">
+            <div className="flex justify-between items-start">
+              <div className="flex flex-col">
+                <p className="text-xl">Accomodation</p>
+                <div className="pt-2 flex flex-row items">
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>
+                        <img
+                          className="rounded-lg w-full h-auto"
+                          src={tokyo2}
+                          alt="Tokyo Tower"
+                        />
+                      </CardTitle>
+                      <CardTitle>Shinagawa Prince Hotel</CardTitle>
+                      {/* <CardDescription>Card Description</CardDescription> */}
+                    </CardHeader>
+                    <CardContent>
+                      <p>Check in: 26.01.2025, 11:15 pm</p>
+                      <p>Check out: 28.01.2025, 11:15 am</p>
+                    </CardContent>
+                    <CardFooter>
+                      <p>2 Nights</p>
+                      <p>Confirmed</p>
+                    </CardFooter>
+                  </Card>
                 </div>
               </div>
               <div>
