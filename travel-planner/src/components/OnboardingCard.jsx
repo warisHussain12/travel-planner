b0569bbc14ba4380c59506493529061f8d9aeab4
Waris Hussain <onlyhectic@gmail.com>
2025-05-08 13:20:25 +0530

    onboarding page v1 complete

--- travel-planner/src/components/OnboardingCard.jsx
+++ travel-planner/src/components/OnboardingCard.jsx
@@ -17,50 +17,100 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { HiOutlineLocationMarker } from "react-icons/hi";
-
+import { CiCalendar } from "react-icons/ci";
+import { Button } from "@/components/ui/button";
+import { FaP, FaPerson } from "react-icons/fa6";
+import { ImManWoman } from "react-icons/im";
+import { MdOutlineFamilyRestroom } from "react-icons/md";
+import { TbFriends } from "react-icons/tb";
 function OnboardingCard() {
   return (
-    <div className="min-h-screen">
-      <Card>
-        <CardHeader className={""}>
-          <CardTitle className={"text-2xl font-bold"}>
-            Plan Your Journey, Your Way!
-          </CardTitle>
-          <CardDescription>
-            Let's create your personalized travel experience
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Where would you like to go?</p>
-          <div className="relative">
-            <HiOutlineLocationMarker className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-800" />
-            <Input
-              type="text"
-              placeholder="Enter destination"
-              className="w-full pl-10 pr-4 py-2 mt-2 border-2 border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-            />
-          </div>
-        </CardContent>
-        <CardContent>
-          <p>How long will you stay?</p>
-          <div className="mt-2">
-            <Select>
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Theme" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </CardContent>
-        <CardContent>
-          <p>Who are you traveling with?</p>
-        </CardContent>
-        <CardFooter>
-          <p></p>
+    <div className="min-h-screen bg-neutral-100 flex items-center justify-center px-2">
+      <Card className="w-full max-w-sm flex flex-col h-full min-h-screen">
+        <div className="flex flex-col flex-grow">
+          <CardHeader className="mb-1">
+            <CardTitle className="text-xl font-bold">
+              Plan Your Journey, Your Way!
+            </CardTitle>
+            <CardDescription>
+              Let's create your personalized travel experience
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="mt-2">Where would you like to go?</p>
+            <div className="relative">
+              <HiOutlineLocationMarker className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-800" />
+              <Input
+                type="text"
+                placeholder="Enter Destination"
+                className="w-full pl-10 pr-2 py-2 mt-2 border-2 border-[#363732] rounded-md focus:outline-none focus:border-blue-500 font-700 placeholder:text-black"
+              />
+            </div>
+          </CardContent>
+
+          <CardContent>
+            <p className="mt-2">How long will you stay?</p>
+            <div className="mt-2 relative">
+              <CiCalendar className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-800" />
+              <Select>
+                <SelectTrigger className="w-full pl-10 pr-2 py-2 mt-2 border-2 border-[#363732] rounded-md focus:outline-none focus:border-blue-500 ">
+                  <SelectValue
+                    placeholder="Select Duration"
+                    className="data-[placeholder]:font-bold"
+                  />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="week">Week</SelectItem>
+                  <SelectItem value="month">Month</SelectItem>
+                  <SelectItem value="year">Year</SelectItem>
+                  <SelectItem value="custom">Custom</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </CardContent>
+
+          <CardContent className="flex-grow">
+            <p className="mt-2">Who are you traveling with?</p>
+            <div className="flex flex-col gap-2 mt-2">
+              <div className="flex gap-2 flex-wrap">
+                <Button
+                  variant="outline"
+                  className="flex-1 min-w-[45%] border-[#6d6d6d]"
+                >
+                  <FaPerson className="mr-1" />
+                  Solo
+                </Button>
+                <Button
+                  variant="outline"
+                  className="flex-1 min-w-[45%] border-[#6d6d6d]"
+                >
+                  <ImManWoman className="mr-1" />
+                  Couple
+                </Button>
+                <Button
+                  variant="outline"
+                  className="flex-1 min-w-[45%] border-[#6d6d6d]"
+                >
+                  <MdOutlineFamilyRestroom className="mr-1" />
+                  Family
+                </Button>
+                <Button
+                  variant="outline"
+                  className="flex-1 min-w-[45%] border-[#6d6d6d]"
+                >
+                  <TbFriends className="mr-1" />
+                  Friends
+                </Button>
+              </div>
+            </div>
+          </CardContent>
+        </div>
+
+        <CardFooter className="px-4 pb-4">
+          <Button className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-medium rounded-lg">
+            Continue
+          </Button>
         </CardFooter>
       </Card>
     </div>
