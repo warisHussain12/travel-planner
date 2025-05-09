266734caa56c93881405ece7804b005e6e2ec579
Waris Hussain <onlyhectic@gmail.com>
2025-05-09 13:45:23 +0530

    added 3 activities

--- travel-planner/src/components/Activities.jsx
+++ travel-planner/src/components/Activities.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import sensoji from "../assets/sensoji.png";
+import tokyosky from "../assets/tokyosky.png";
+import kimono from "../assets/kimono.png";
+import { MdOutlineNordicWalking } from "react-icons/md";
 import {
   Card,
   CardContent,
@@ -16,7 +20,6 @@ function Activities() {
           <p className="text-xl">Activities</p>
           <p className="underline text-xs whitespace-nowrap">See all</p>
         </div>
-
         <div className=" px-2 py-3 border-1 border-[#8d99ae] rounded-lg w-full max-w-sm min-h-[100px]">
           <div className="flex flex-row flex-wrap justify-start gap-2">
             <div className="border-2 border-[#313ddf] rounded-sm px-3 py-1 bg-[#313ddf]">
@@ -61,6 +64,83 @@ function Activities() {
                   <p>SAT</p>
                   <p className="font-bold">1</p>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="py-3 rounded-lg w-full max-w-sm flex flex-row items-start gap-2">
+          <div className="bg-[#313ddf] text-white rounded-lg p-1 min-w-[31%]">
+            <p className="text-xs font-bold">Day 1 27.01.2025</p>
+          </div>
+          <div className="flex flex-row rounded-lg p-1 min-w-[31%]">
+            <MdOutlineNordicWalking className="text-[#313ddf]" />
+            <p className="text-xs text-[#313ddf] font-semibold">3 Activities</p>
+          </div>
+        </div>
+        {/* Activity1 */}
+        <div className="pr-2 mb-2 border-1 border-[#8d99ae] rounded-lg w-full max-w-sm">
+          <div className="flex flex-row gap-0 items-center min-h-[127px]">
+            <img className="rounded-l-lg" src={sensoji} alt="Senso-Ji" />
+            <div className="px-1 py-1 bg-[#ffffff] w-full h-[127px] rounded-r-sm flex flex-col justify-between leading-snug">
+              <div>
+                <p className="text-md font-semibold">
+                  Senso-ji Temple & Nakamise Shopping Street Senso-ji
+                </p>
+              </div>
+              <div>
+                <p className="text-xs">
+                  <span className="font-semibold">Timing</span>: 8:15 am Morning
+                </p>
+                <p className="text-xs text-bold">
+                  <span className="font-semibold">Duration</span>: 3 hours
+                </p>
+                <p className="text-xs text-bold">
+                  <span className="font-semibold">Pick up</span>: From Hotel
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Activity2 */}
+        <div className="pr-2 mb-2 border-1 border-[#8d99ae] rounded-lg w-full max-w-sm">
+          <div className="flex flex-row gap-0 items-center min-h-[127px]">
+            <img className="rounded-l-lg" src={tokyosky} alt="Senso-Ji" />
+            <div className="px-1 py-4 bg-[#ffffff] w-full h-[127px] rounded-r-sm flex flex-col justify-between">
+              <div className="">
+                <p className="text-md font-semibold">Tokyo Sky Tree</p>
+              </div>
+              <div>
+                <p className="text-xs">
+                  <span className="font-semibold">Timing</span>: 8:15 am Morning
+                </p>
+                <p className="text-xs text-bold">
+                  <span className="font-semibold">Duration</span>: 3 hours
+                </p>
+                <p className="text-xs text-bold">
+                  <span className="font-semibold">Pick up</span>: From Hotel
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Activity3 */}
+        <div className="pr-2 mb-2 border-1 border-[#8d99ae] rounded-lg w-full max-w-sm">
+          <div className="flex flex-row gap-0 items-center min-h-[127px]">
+            <img className="rounded-l-lg" src={kimono} alt="Senso-Ji" />
+            <div className="px-1 py-4 bg-[#ffffff] w-full h-[127px] rounded-r-sm flex flex-col justify-between">
+              <div className="">
+                <p className="text-md font-semibold">Kimono Wearing</p>
+              </div>
+              <div>
+                <p className="text-xs">
+                  <span className="font-semibold">Timing</span>: 8:15 am Morning
+                </p>
+                <p className="text-xs text-bold">
+                  <span className="font-semibold">Duration</span>: 3 hours
+                </p>
+                <p className="text-xs text-bold">
+                  <span className="font-semibold">Pick up</span>: From Hotel
+                </p>
               </div>
             </div>
           </div>
